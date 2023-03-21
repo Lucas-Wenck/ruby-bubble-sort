f5fb89b6633d4c97320e054fbd5590706623b4e0
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-20 23:27:40 -0300

    Create loop to check and change the array elements

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,4 +1,18 @@
 def bubble_sort(array)
     amount = array.length
-    
+    loop do
+        swap = false
+
+        (amount - 1).times do |i|
+            if array[i] > array[i+1]
+                array[i], array[i+1] = array[i+1], array[i]
+                swap = true
+            end
+        end
+        break unless swap
+    end
+
+    p array
 end
+
+bubble_sort([4, 3, 78, 2, 0, 2])
